3487fafccf2d9e62f4579a1a992479bcb2c9764d
KirylZuyeu <kirylzuyeuimh@gmail.com>
2020-03-10 19:53:34 +0300

    init:add a decision of Bracets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let items = bracketsConfig.map( item => item.join(''));
+    let string = str;
+
+    function rebase(string) {
+        for (let i =0; i < items.length; i++) {
+            if (string.includes(items[i])) {
+                string = string.replace(items[i], '');
+                return rebase(string);
+            } else if (!string.includes(items[i]) && string.length == 0) {
+                return true;
+            } else if (!string.includes(items[i]) && string.length < 0) {
+                return false;
+            }
+        }
+        return false;
+    }
+    return rebase(str);
 }
